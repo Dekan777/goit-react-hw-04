4a3567f884203a3da26a98a81fdc77280b2ccfb2
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-26 02:32:39 +0100

    css btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
 // import { useState, useEffect } from 'react';
 // import { nanoid } from 'nanoid';
-
+import { SearchBar } from './SearchBar/SearchBar';
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import css from './SearchBar.module.css';
+
+//функцію для передачі значення інпуту під час сабміту форми
+export const SearchBar = () => {
+  const inputRef = useRef();
+  const handleSumbit = evt => {
+    evt.preventDefault();
+    const inputValue = inputRef.current.value;
+
+    if (!inputValue.trim()) {
+      toast.error('Please enter a value');
+      return;
+    }
+    evt.target.reset();
+  };
+
+  return (
+    <>
+      <header>
+        <form onSubmit={handleSumbit} className={css.searchContainer}>
+          <div className={css.formContainer}>
+            <div className={css.inputContainer}>
+              <input
+                ref={inputRef}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+                className={css.inputField}
+              />
+              <button type="submit" className={css.submitButton}>
+                Search
+              </button>
+            </div>
+          </div>
+          <Toaster containerStyle={{ marginTop: 70 }} />
+        </form>
+      </header>
+    </>
+  );
+};
